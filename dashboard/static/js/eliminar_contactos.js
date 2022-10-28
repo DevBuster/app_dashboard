dc18747e853cc87bb7a5aaa3b09db7599a8bf465
DevBuster <58188506+DevBuster@users.noreply.github.com>
2022-10-28 10:19:49 -0500

    sweet alert 2 implementado

--- dashboard/static/js/eliminar_contactos.js
+++ dashboard/static/js/eliminar_contactos.js
@@ -1,14 +1,40 @@
+const btn_eliminar_contactos = document.querySelectorAll(".btn_eliminar_contactos");
+
 (function () {
-    // ------- ACCION DEL BOTON "ELIMINAR" CONTACTOS ------- //
-    const btn_eliminar_contactos = document.querySelectorAll(".btn_eliminar_contactos");
 
-    btn_eliminar_contactos.forEach(btn_eliminar_contactos => {
-        btn_eliminar_contactos.addEventListener("click", (evento) => {
-            const confirmar_eliminar_contactos = confirm("¿Seguro(a) que quiere eliminar éste registro?");
+    // notificacionSwal("LoadQuery", "Registros cargados con exito", "success", "Ok");
 
-            if (!confirmar_eliminar_contactos) {
-                evento.preventDefault();
-            }
-        });
+    btn_eliminar_contactos.forEach(btn => {
+        btn.addEventListener('click', function (e) {
+            e.preventDefault();
+            Swal.fire({
+                titleText: "ELIMINAR REGISTRO",
+                text: "¿Segur@ que quiere eliminar este registro?",
+                icon: "warning", // warnign, error, success, info
+                showCancelButton: true,
+                confirmButtonColor: "#d33",
+                backdrop: true,
+                showLoaderOnConfirm: true,
+                confirmButtonText: "OK",
+                preConfirm: () => {
+                    location.href = e.target.href;
+                },
+                allowOutsideClick: () => false,
+                allowEscapeKey: () => false,
+            })
+        })
     });
+
+    // ------- ACCION DEL BOTON "ELIMINAR" CONTACTOS ------- //
+    // const btn_eliminar_contactos = document.querySelectorAll(".btn_eliminar_contactos");
+
+    // btn_eliminar_contactos.forEach(btn_eliminar_contactos => {
+    //     btn_eliminar_contactos.addEventListener("click", (evento) => {
+    //         const confirmar_eliminar_contactos = confirm("¿Seguro(a) que quiere eliminar éste registro?");
+
+    //         if (!confirmar_eliminar_contactos) {
+    //             evento.preventDefault();
+    //         }
+    //     });
+    // });
 })();

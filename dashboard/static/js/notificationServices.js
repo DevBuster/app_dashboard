b0979f8c50994477f8d1e45f80c51070713c0344
DevBuster <58188506+DevBuster@users.noreply.github.com>
2022-10-29 18:28:43 -0500

    modal de grafica

--- dashboard/static/js/notificationServices.js
+++ dashboard/static/js/notificationServices.js
@@ -1,16 +1,21 @@
 const btn_eliminar_registro = document.querySelectorAll(".btn_eliminar_registro");
 const btn_guardar_registro = document.querySelectorAll(".btn_guardar_registro");
 const btn_actualizar_registro = document.querySelectorAll(".btn_actualizar_registro");
+const btn_generar_grafica = document.querySelectorAll(".btn_generar_grafica");
+
+
+let modalGraficaBarra = document.getElementById('modalGraficaBarra');
+modalGraficaBarra.style.display = 'none';
 
 (function () {
 
     const swalWithBootstrapButtons = Swal.mixin({
         customClass: {
-          confirmButton: 'btn btn-success',
-          cancelButton: 'btn btn-danger'
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
         },
         buttonsStyling: false
-      });
+    });
     // notificacionSwal("LoadQuery", "Registros cargados con exito", "success", "Ok");
 
     btn_guardar_registro.forEach(btn => {
@@ -80,6 +85,50 @@ const btn_actualizar_registro = document.querySelectorAll(".btn_actualizar_regis
                         'Su registro está intacto :)',
                         'error'
                     )
+                }
+            })
+        })
+    });
+
+    btn_guardar_registro.forEach(btn => {
+        btn.addEventListener('click', function () {
+            Swal.fire({
+                titleText: "REGISTRO CREADO",
+                text: "Nice!!",
+                icon: "success", // warnign, error, success, info
+                confirmButtonColor: "green",
+                backdrop: true,
+                showLoaderOnConfirm: true,
+                confirmButtonText: "Nice!",
+                allowOutsideClick: () => false,
+                allowEscapeKey: () => false,
+            })
+        })
+    });
+
+    // generar grafica de barra
+    btn_generar_grafica.forEach(btn => {
+        btn.addEventListener('click', function () {
+            let timerInterval
+            Swal.fire({
+                title: 'Grafica de barra!',
+                html: 'Generando grafica de barra...<b></b> milliseconds.',
+                timer: 2000,
+                timerProgressBar: true,
+                didOpen: () => {
+                    Swal.showLoading()
+                    const b = Swal.getHtmlContainer().querySelector('b')
+                    timerInterval = setInterval(() => {
+                        b.textContent = Swal.getTimerLeft()
+                    }, 100)
+                },
+                willClose: () => {
+                    clearInterval(timerInterval)
+                }
+            }).then((result) => {
+                /* Read more about handling dismissals below */
+                if (result.dismiss === Swal.DismissReason.timer) {
+                    modalGraficaBarra.style.display = 'block';
                 }
             })
         })
